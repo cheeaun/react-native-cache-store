a24aab61b57f4d11660de0205af4481d4a4e7132
Lim Chee Aun <cheeaun@techinasia.com>
2016-04-03 23:45:20 +0800

    Finally manage to make tests work!

---
+++ __mocks__/react-native.js
@@ -0,0 +1,67 @@
+const React = require('react');
+const ReactNative = React;
+
+// Some code here is inspired by https://github.com/lelandrichardson/react-native-mock/
+
+function wrap(value){
+  return Promise.resolve(value);
+};
+
+let db = {};
+
+const AsyncStorage = {
+  getItem(key, callback){
+    return wrap(db[key]);
+  },
+
+  setItem(key, value, callback){
+    db[key] = value;
+    return wrap(null);
+  },
+
+  removeItem(key, callback){
+    delete db[key];
+    return wrap(null);
+  },
+
+  mergeItem(key, value, callback){
+    db[key] = Object.assign({}, db[key] || {}, value);
+    return wrap(null);
+  },
+
+  clear(callback){
+    db = {};
+    return wrap(null);
+  },
+
+  getAllKeys(callback){
+    return wrap(Object.keys(db));
+  },
+
+  multiGet(keys, callback){
+    return wrap(keys.map(k => [k, db[k]]));
+  },
+
+  multiSet(keyValuePairs, callback){
+    keyValuePairs.forEach(([key, value]) => {
+      db[key] = value;
+    });
+    return wrap(null);
+  },
+
+  multiRemove(keys, callback){
+    keys.forEach(key => delete db[key]);
+    return wrap(null);
+  },
+
+  multiMerge(keyValuePairs, callback){
+    keyValuePairs.forEach(([key, value]) => {
+      db[key] = Object.asign({}, db[key] || {}, value);
+    });
+    return wrap(null);
+  },
+};
+
+ReactNative.AsyncStorage = AsyncStorage;
+
+module.exports = ReactNative;
--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -3,29 +3,14 @@
 import CacheStore from '../index';
 jest.unmock('../index');
 
-let cache = {};
-jest.setMock('react-native', {
-  AsyncStorage: {
-    setItem: jest.genMockFunction().mockImplementation((key, value) => {
-      return new Promise((resolve, reject) => {
-        if(typeof key !== 'string' || typeof value !== 'string'){
-          reject(new Error('key and value must be string'));
-        }
-        resolve(cache[key] = value);
-      });
-    }),
-  }
-});
-
 describe('CacheStore', () => {
-  it('calls into AsyncStorage.setItem with the correct params', () => {
-    // I have no idea what I'm doing here
-    console.log(CacheStore);
-    const {AsyncStorage} = require('react-native');
-
+  pit('sets and gets value', () => {
     expect(CacheStore.set).toBeDefined();
+    expect(CacheStore.get).toBeDefined();
 
     CacheStore.set('key', 'value');
-    expect(AsyncStorage.setItem).toBeCalled();
+    return CacheStore.get('key').then((value) => {
+      expect(value).toBe('value');
+    });
   });
 });
