df043d48d61529cae644c3c00ad275e58fd9db61
zacksleo <zacksleo@gmail.com>
2018-12-24 17:57:32 +0800

    travis(ci): update jest test

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -4,7 +4,7 @@ import CacheStore from '../index';
 jest.unmock('../index');
 
 describe('CacheStore', () => {
-  pit('sets and gets value', () => {
+  test('sets and gets value', () => {
     expect(CacheStore.set).toBeDefined();
     expect(CacheStore.get).toBeDefined();
 
@@ -13,4 +13,37 @@ describe('CacheStore', () => {
       expect(value).toBe('value');
     });
   });
+
+  test('test remove', async () => {
+    expect(CacheStore.remove).toBeDefined();
+    await CacheStore.remove('key');
+    CacheStore.get('key').catch((value) => {
+      expect(value).toBeNull();
+    });
+  });
+
+  test('test isExpired', async () => {
+    expect(CacheStore.isExpired).toBeDefined();
+    CacheStore.set('isExpired', 'expiredValue', 2).then(() => {
+      CacheStore.isExpired('isExpired').catch(() => {
+        CacheStore.get('isExpired').then((value) => {
+          expect(value).toBe('expiredValue');
+        });
+      });
+    });
+  });
+
+  test('test flush', async () => {
+    await CacheStore.set('flushBefore', 'exists');
+    expect(CacheStore.flush).toBeDefined();
+    await CacheStore.flush();
+    CacheStore.get('flushBefore').then((value) => {
+      expect(value).toBeNull();
+    });
+  });
+
+  test('test flushExpired', () => {
+    expect(CacheStore.flushExpired).toBeDefined();
+  });
+
 });
